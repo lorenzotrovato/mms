02ea56423a1bdc401a5bc01fa0a25896a6b81d59
Valerio Bucci <valbuxvb@gmail.com>
2018-04-05 09:58:26 +0200

    Create User.php

---
+++ User.php
@@ -0,0 +1,19 @@
+<?php
+    class User{
+        
+        private $db;
+        
+        private $id;
+        private $user;
+        private $mail;
+        private $role;
+        
+        function __construct($userId){
+                $
+        }
+        
+        function getId(){
+            
+        }
+    }
+?>
