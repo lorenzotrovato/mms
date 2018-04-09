c2f4fba5234726a980b7be3bb34ac07d4d291a5b
andr99 <36842101+andr99@users.noreply.github.com>
2018-04-09 11:57:58 +0200

    Update Biglietto.php

--- src/Biglietto.php
+++ src/Biglietto.php
@@ -5,44 +5,53 @@
 	
 	class Biglietto{
 		private $db;
-        
-        private $id;
-        private $codUser;
-        private $codCat;
-        private $codTimeSlot;
-        private $datePurchase;
-        private $dateValidity;
-        private $totalPrice;
-        
-        public __construct($id){
-            
-        }
-        
-        public getId(){
-            return $this->id;
-        }
-        
-        public getCodUser(){
-            return $this->codUser;
-        }
-        
-        public getCodCat(){
-            return $this->codCat;
-        }
-        
-        public getCodTimeSlot(){
-            return $this->codTimeSlot;
-        }
-        
-        public getDatePurchase(){
-            return $this->datePurchase;
-        }
-        
-        public getDateValidity(){
-            return $this->dateValidity;
-        }
-        public getTotalPrice(){
-            return $this->totalPrice;
-        }
-	}
+
+		private $id;
+		private $codUser;
+		private $codCat;
+		private $codTimeSlot;
+		private $datePurchase;
+		private $dateValidity;
+		private $totalPrice;
+
+		public __construct($id){
+			$this->db = DB::init();
+			$biglietto = $this->db->querySelect("select * from biglietto where id=".$id);
+			$this->id = $biglietto['id'];
+			$this->codUser = $biglietto['codUser'];
+			$this->codCat = $biglietto['codCat'];
+			$this->codTimeSlot = $biglietto['codTimeSlot'];
+			$this->datePurchase = $biglietto['datePurchase'];
+			$this->dateValidity = $biglietto['dateValidity'];
+			$this->totalPrice = $biglietto['totalPrice'];
+		}
+
+		public getId(){
+		    return $this->id;
+		}
+
+		public getCodUser(){
+		    return $this->codUser;
+		}
+
+		public getCodCat(){
+		    return $this->codCat;
+		}
+
+		public getCodTimeSlot(){
+		    return $this->codTimeSlot;
+		}
+
+		public getDatePurchase(){
+		    return $this->datePurchase;
+		}
+
+		public getDateValidity(){
+		    return $this->dateValidity;
+		}
+		
+		public getTotalPrice(){
+		    return $this->totalPrice;
+		}
+	}//class
 ?>
