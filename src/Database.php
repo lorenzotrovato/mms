020dee8625df101d149fc2454bf5fed85903c4a1
Lorenzo Trovato <lorenzo.trovato3@live.com>
2018-04-09 10:39:41 +0200

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -3,7 +3,39 @@
         private $mysqli;
         
         public function __construct(){
-            
+            $this->mysqli= new mysqli('localhost','5ia22','5ia22','5ia22');
+        }
+        
+        public function querySelect($query){
+            $ret= array();
+            try{
+                $ris= $this->mysqli->query($query);
+                return $ris->fecth_all(MYSQLI_ASSOC);
+            }catch(Exception $e){
+                return $this->mysqli->error;
+            }
+        }
+        
+        public function queryDML($query){
+           try{
+                $ris= $this->mysqli->query($query);
+                return $this->mysqli->affectedRows;
+            }catch(Exception $e){
+                return $this->mysqli->error;
+            }
+        }
+        
+        public function numRows($query){
+            try{
+                $ris= $this->mysqli->query($query);
+                return $ris->num_rows;
+            }catch(Exception $e){
+                return $this->mysqli->error;
+            }
+        }
+        
+        public function error(){
+            return $this->mysqli->error;
         }
     }
 ?>
