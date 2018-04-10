70ab21923fae5de10f38bea9f05f4a130a85ae9c
Valerio Bucci <valbuxvb@gmail.com>
2018-04-10 10:47:11 +0200

    Sistemato il costruttore
    Manca l'array di face orarie

--- src/Visit.php
+++ src/Visit.php
@@ -1,11 +1,29 @@
 <?php
-    namespace MMS;
-    require 'Database.php';
-    use MMS\Database as DB;
-    
-    class Visit{
-        public function __construct(){
-            $mysqli = DB::init();
-        }
-    }
+	namespace MMS;
+	require 'Database.php';
+	use MMS\Database as DB;
+
+	class Visit{
+		private $mysqli;
+
+		private $id;
+		private $name;
+		private $startDate;
+		private $endDate;
+		private $price;
+		private $maxSeats;
+
+		public function __construct(){
+			$this->mysqli = DB::init();
+			$sql = "SELECT * FROM evento WHERE id=0";
+			$row = $mysqli->querySelect($sql)[0];
+
+			$this->id = 0;
+			$this->name = $row['name'];
+			$this->startDate = $row['startDate'];
+			$this->endDate = $row['endDate'];
+			$this->price = $row['price'];
+			$this->maxSeats = $row['maxSeats'];
+		}
+	}
 ?>
