f1ae03ae53946767f35e8b066e1c13d1a176bc30
Mattiam2 <novembre1998@libero.it>
2018-04-17 08:47:33 +0000

    COSE

--- src/index.php
+++ src/index.php
@@ -6,13 +6,13 @@
 		<meta name="description" content="">
 		<meta name="author" content="">
 
-		<title>Cover Template for Bootstrap</title>
+		<title>Musetek - Innovation of the past</title>
 
 		<!-- Bootstrap core CSS -->
 		<link href="./css/bootstrap.min.css" rel="stylesheet">
 
 		<!-- Custom styles for this template -->
-		<link href="./cover.css" rel="stylesheet">
+		<link href="./css/cover.css" rel="stylesheet">
 	</head>
 
 	<body class="text-center">
@@ -20,26 +20,26 @@
 		<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
 			<header class="masthead mb-auto">
 				<div class="inner">
-					<h3 class="masthead-brand">Cover</h3>
+					<h3 class="masthead-brand">MuseTek</h3>
 					<nav class="nav nav-masthead justify-content-center">
 						<a class="nav-link active" href="#">Home</a>
-						<a class="nav-link" href="#">Features</a>
-						<a class="nav-link" href="#">Contact</a>
+						<a class="nav-link" href="#">Biglietti</a>
+						<a class="nav-link" href="#">Eventi</a>
 					</nav>
 				</div>
 			</header>
 
 			<main role="main" class="inner cover">
-				<h1 class="cover-heading">Cover your page.</h1>
-				<p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+				<h1 class="cover-heading">MuseTek</h1>
+				<p class="lead">Vieni a trovarci, e scopri l'innovazione del passato!</p>
 				<p class="lead">
-					<a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+					<a href="#" class="btn btn-lg btn-secondary">Mocc' a mammt'</a>
 				</p>
 			</main>
 
 			<footer class="mastfoot mt-auto">
 				<div class="inner">
-					<p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+					<p>Se volete comprare la droga passate dagli <span class="text-white">Arabacci</span>.</p>
 				</div>
 			</footer>
 		</div>
